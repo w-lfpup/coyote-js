b61e9e00fcf6f831e424154ebeda706d49e0f005
Taylor Vann <brian.t.vann@gmail.com>
2024-12-05 15:51:18 -0800

    add component_string

---
+++ component_string/src/mod.ts
@@ -0,0 +1,11 @@
+import type { StepInterface, StepKind } from "../../parse_str/dist/mod.ts";
+import type { RulesetInterface } from "../../rulesets/dist/mod.ts";
+import type { Results } from "../../parse_str/dist/mod.ts";
+
+function compose(ruleset: RulesetInterface, templateStr: string): string {
+	let results = [];
+
+	return results.join("");
+}
+
+export { compose };
