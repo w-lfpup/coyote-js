bd2017f701a665817d4950d0c9647e69799db270
Taylor Vann <brian.t.vann@gmail.com>
2024-12-06 01:37:34 -0800

    export html

--- coyote_html/src/mod.ts
+++ coyote_html/src/mod.ts
@@ -46,4 +46,4 @@ export type { RulesetInterface } from "../../rulesets/dist/mod.ts";
 export { compose as prettyHtml } from "../../html/dist/mod.js";
 export { ClientRules, ServerRules } from "../../rulesets/dist/mod.js";
 
-export { Builder };
+export { Builder, Html };
