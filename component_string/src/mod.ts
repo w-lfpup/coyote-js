6199164aa6e6c6b215a6fe1e1d7ea6d465926af9
Taylor Vann <brian.t.vann@gmail.com>
2024-12-05 19:17:21 -0800

    component_string prep

--- component_string/src/mod.ts
+++ component_string/src/mod.ts
@@ -1,6 +1,14 @@
 import type { StepInterface, StepKind } from "../../parse_str/dist/mod.ts";
 import type { RulesetInterface } from "../../rulesets/dist/mod.ts";
+import type { Component } from "../../coyote/dist/mod.ts";
 
+import {
+	CoyoteComponent,
+	TmplComponent,
+	TaggedTmplComponent,
+	AttrComponent,
+	AttrValComponent,
+} from "../../coyote/dist/mod.js";
 import { Results as ParseResults } from "../../parse_str/dist/mod.js";
 
 interface BuilderInterface {
@@ -11,16 +19,63 @@ class TemplateBit {
 	injIndex = 0;
 }
 
-class StackBit {}
-class TmplStackBit extends StackBit {}
-class CmpntStackBit extends StackBit {}
-
-type StackBit = Component | TmplComponent | undefined;
-
-function compose(ruleset: RulesetInterface, templateStr: string): string {
+function compose(
+	builder: BuilderInterface,
+	ruleset: RulesetInterface,
+	component: CoyoteComponent,
+) {
 	let results = [];
 
+	let bit = getStackBitFromComponent(builder, ruleset, component);
+	let stack = [bit];
+
 	return results.join("");
+}
+
+function getStackBitFromComponent(
+	builder: BuilderInterface,
+	rules: RulesetInterface,
+	component: CoyoteComponent,
+): Component {
+	if (typeof component === "string" || Array.isArray(component))
+		return component;
+
+	if (component instanceof TmplComponent) {
+		// build template return tmplate_bit
+	}
+
+	if (component instanceof TaggedTmplComponent) {
+		// build template return tmplate_bit
+	}
+}
+
+function addAttrInj(results: string[], component: Component) {
+	if (component instanceof AttrComponent)
+		return addAttr(results, component.attr);
+	if (component instanceof AttrValComponent)
+		return addAttrVal(results, component.attr, component.value);
+
+	if (Array.isArray(component)) {
+		for (const cmpnt of component) {
+			if (component instanceof AttrComponent)
+				return addAttr(results, component.attr);
+			if (component instanceof AttrValComponent)
+				return addAttrVal(results, component.attr, component.value);
+		}
+	}
+}
+
+function addAttr(results: string[], attr: string) {
+	results.push(" ");
+	results.push(attr);
+}
+
+function addAttrVal(results: string[], attr: string, val: string) {
+	results.push(" ");
+	results.push(attr);
+	results.push('="');
+	results.push(val);
+	results.push('"');
 }
 
 export { compose };
