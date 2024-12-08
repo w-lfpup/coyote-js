c5f7beb548aba6e12e1a9e61f2874e245464dfce
Taylor Vann <brian.t.vann@gmail.com>
2024-12-08 14:09:18 -0800

    basic dx of dom packages

--- component_dom/src/mod.ts
+++ component_dom/src/mod.ts
@@ -1,11 +1,32 @@
 // take builder for template strings
 
+import { Results as TemplateResults } from "../../template_str/dist/mod.js";
+
 // then make builder for DOM from template_str builder results
+interface Results {
+	fragment: Node;
+	injs: number[][];
+}
+
+interface BuilderInterface {
+	build(templateResults: TemplateResults): Results;
+}
 
 // the "stack" is basically an object:
 interface StackBit {
-	parent: Node;
-	left: Node;
-	dom: Node[];
+	parent?: Node;
+	left?: Node;
+	dom: Node[][];
 	injections: number[];
 }
+
+function compose(): Node {
+	// create and return a document fragment
+	let fragment = new DocumentFragment();
+
+	return fragment;
+}
+
+export type { Results, BuilderInterface };
+
+export { compose };
---
+++ coyote_dom/src/mod.ts
@@ -0,0 +1,33 @@
+import { Component } from "../../coyote/dist/mod.js";
+import { Results as TemplateResults } from "../../template_str/dist/mod.js";
+import type {
+	Results,
+	BuilderInterface,
+} from "../../component_dom/dist/mod.ts";
+
+import { compose } from "../../component_dom/dist/mod.js";
+
+class Builder implements BuilderInterface {
+	// place to add cache
+
+	build(results: TemplateResults): Results {
+		return {
+			fragment: new DocumentFragment(),
+			injs: [],
+		};
+	}
+}
+
+class Dom {
+	builder: BuilderInterface;
+
+	constructor(builder: BuilderInterface) {
+		this.builder = builder;
+	}
+
+	build(component: Component): Node {
+		return compose();
+	}
+}
+
+export { Builder, Dom };
