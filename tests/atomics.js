4ff7465002158a6f1444ec2f9598001c514c950e
Taylor Vann <brian.t.vann@gmail.com>
2026-05-28 22:43:02 -0700

    don't format generated code

--- tests/atomics.js
+++ tests/atomics.js
@@ -5,142 +5,157 @@ import * as acsl from "./atomics_component_literals_set.js";
 import { compose } from "../dist/template_steps/template_steps.js";
 const coyote = new Coyote();
 function text_element() {
-	let expected = "Beasts tread\nsoftly underfoot.";
-	let assertions = [];
-	let results = coyote.render(acs.text_element());
-	let templateAssertion = assert(expected, results);
-	if (templateAssertion) assertions.push(templateAssertion);
-	let literal_results = coyote.render(acsl.text_element());
-	let literalAssertion = assert(expected, literal_results);
-	if (literalAssertion) assertions.push(literalAssertion);
-	if (results[0] !== literal_results[0])
-		assertions.push(`literal does not match string`);
-	return assertions;
+    let expected = "Beasts tread\nsoftly underfoot.";
+    let assertions = [];
+    let results = coyote.render(acs.text_element());
+    let templateAssertion = assert(expected, results);
+    if (templateAssertion)
+        assertions.push(templateAssertion);
+    let literal_results = coyote.render(acsl.text_element());
+    let literalAssertion = assert(expected, literal_results);
+    if (literalAssertion)
+        assertions.push(literalAssertion);
+    if (results[0] !== literal_results[0])
+        assertions.push(`literal does not match string`);
+    return assertions;
 }
 function empty_element() {
-	let expected = "<p>\n</p>";
-	let assertions = [];
-	let results = coyote.render(acs.empty_element());
-	let templateAssertion = assert(expected, results);
-	if (templateAssertion) assertions.push(templateAssertion);
-	let literal_results = coyote.render(acsl.empty_element());
-	let literalAssertion = assert(expected, literal_results);
-	if (literalAssertion) assertions.push(literalAssertion);
-	if (results[0] !== literal_results[0])
-		assertions.push(`literal does not match string`);
-	return assertions;
+    let expected = "<p>\n</p>";
+    let assertions = [];
+    let results = coyote.render(acs.empty_element());
+    let templateAssertion = assert(expected, results);
+    if (templateAssertion)
+        assertions.push(templateAssertion);
+    let literal_results = coyote.render(acsl.empty_element());
+    let literalAssertion = assert(expected, literal_results);
+    if (literalAssertion)
+        assertions.push(literalAssertion);
+    if (results[0] !== literal_results[0])
+        assertions.push(`literal does not match string`);
+    return assertions;
 }
 function fragment() {
-	let expected = "";
-	let assertions = [];
-	let results = coyote.render(acs.fragment());
-	let templateAssertion = assert(expected, results);
-	if (templateAssertion) assertions.push(templateAssertion);
-	let literal_results = coyote.render(acsl.fragment());
-	let literalAssertion = assert(expected, literal_results);
-	if (literalAssertion) assertions.push(literalAssertion);
-	if (results[0] !== literal_results[0])
-		assertions.push(`literal does not match string`);
-	return assertions;
+    let expected = "";
+    let assertions = [];
+    let results = coyote.render(acs.fragment());
+    let templateAssertion = assert(expected, results);
+    if (templateAssertion)
+        assertions.push(templateAssertion);
+    let literal_results = coyote.render(acsl.fragment());
+    let literalAssertion = assert(expected, literal_results);
+    if (literalAssertion)
+        assertions.push(literalAssertion);
+    if (results[0] !== literal_results[0])
+        assertions.push(`literal does not match string`);
+    return assertions;
 }
 function block_element_with_text() {
-	let expected = "<p>\n\thello!\n</p>";
-	let assertions = [];
-	let results = coyote.render(acs.block_element_with_text());
-	let templateAssertion = assert(expected, results);
-	if (templateAssertion) assertions.push(templateAssertion);
-	let literal_results = coyote.render(acsl.block_element_with_text());
-	let literalAssertion = assert(expected, literal_results);
-	if (literalAssertion) assertions.push(literalAssertion);
-	if (results[0] !== literal_results[0])
-		assertions.push(`literal does not match string`);
-	return assertions;
+    let expected = "<p>\n\thello!\n</p>";
+    let assertions = [];
+    let results = coyote.render(acs.block_element_with_text());
+    let templateAssertion = assert(expected, results);
+    if (templateAssertion)
+        assertions.push(templateAssertion);
+    let literal_results = coyote.render(acsl.block_element_with_text());
+    let literalAssertion = assert(expected, literal_results);
+    if (literalAssertion)
+        assertions.push(literalAssertion);
+    if (results[0] !== literal_results[0])
+        assertions.push(`literal does not match string`);
+    return assertions;
 }
 function inline_element_with_text() {
-	let expected = "<b> hello! </b>";
-	let assertions = [];
-	let results = coyote.render(acs.inline_element_with_text());
-	let templateAssertion = assert(expected, results);
-	if (templateAssertion) assertions.push(templateAssertion);
-	let literal_results = coyote.render(acsl.inline_element_with_text());
-	let literalAssertion = assert(expected, literal_results);
-	if (literalAssertion) assertions.push(literalAssertion);
-	if (results[0] !== literal_results[0])
-		assertions.push(`literal does not match string`);
-	return assertions;
+    let expected = "<b> hello! </b>";
+    let assertions = [];
+    let results = coyote.render(acs.inline_element_with_text());
+    let templateAssertion = assert(expected, results);
+    if (templateAssertion)
+        assertions.push(templateAssertion);
+    let literal_results = coyote.render(acsl.inline_element_with_text());
+    let literalAssertion = assert(expected, literal_results);
+    if (literalAssertion)
+        assertions.push(literalAssertion);
+    if (results[0] !== literal_results[0])
+        assertions.push(`literal does not match string`);
+    return assertions;
 }
 function void_element() {
-	let expected = "<input>";
-	let assertions = [];
-	let results = coyote.render(acs.void_element());
-	let templateAssertion = assert(expected, results);
-	if (templateAssertion) assertions.push(templateAssertion);
-	let literal_results = coyote.render(acsl.void_element());
-	let literalAssertion = assert(expected, literal_results);
-	if (literalAssertion) assertions.push(literalAssertion);
-	if (results[0] !== literal_results[0])
-		assertions.push(`literal does not match string`);
-	return assertions;
+    let expected = "<input>";
+    let assertions = [];
+    let results = coyote.render(acs.void_element());
+    let templateAssertion = assert(expected, results);
+    if (templateAssertion)
+        assertions.push(templateAssertion);
+    let literal_results = coyote.render(acsl.void_element());
+    let literalAssertion = assert(expected, literal_results);
+    if (literalAssertion)
+        assertions.push(literalAssertion);
+    if (results[0] !== literal_results[0])
+        assertions.push(`literal does not match string`);
+    return assertions;
 }
 function void_element_with_self_closing() {
-	let expected = "<input>";
-	let assertions = [];
-	let results = coyote.render(acs.void_element_with_self_closing());
-	let templateAssertion = assert(expected, results);
-	if (templateAssertion) assertions.push(templateAssertion);
-	let literal_results = coyote.render(acsl.void_element_with_self_closing());
-	let literalAssertion = assert(expected, literal_results);
-	if (literalAssertion) assertions.push(literalAssertion);
-	if (results[0] !== literal_results[0])
-		assertions.push(`literal does not match string`);
-	return assertions;
+    let expected = "<input>";
+    let assertions = [];
+    let results = coyote.render(acs.void_element_with_self_closing());
+    let templateAssertion = assert(expected, results);
+    if (templateAssertion)
+        assertions.push(templateAssertion);
+    let literal_results = coyote.render(acsl.void_element_with_self_closing());
+    let literalAssertion = assert(expected, literal_results);
+    if (literalAssertion)
+        assertions.push(literalAssertion);
+    if (results[0] !== literal_results[0])
+        assertions.push(`literal does not match string`);
+    return assertions;
 }
 function non_void_element() {
-	let expected = "<p></p>";
-	let assertions = [];
-	let results = coyote.render(acs.non_void_element());
-	let templateAssertion = assert(expected, results);
-	if (templateAssertion) assertions.push(templateAssertion);
-	let literal_results = coyote.render(acsl.non_void_element());
-	let literalAssertion = assert(expected, literal_results);
-	if (literalAssertion) assertions.push(literalAssertion);
-	if (results[0] !== literal_results[0])
-		assertions.push(`literal does not match string`);
-	return assertions;
+    let expected = "<p></p>";
+    let assertions = [];
+    let results = coyote.render(acs.non_void_element());
+    let templateAssertion = assert(expected, results);
+    if (templateAssertion)
+        assertions.push(templateAssertion);
+    let literal_results = coyote.render(acsl.non_void_element());
+    let literalAssertion = assert(expected, literal_results);
+    if (literalAssertion)
+        assertions.push(literalAssertion);
+    if (results[0] !== literal_results[0])
+        assertions.push(`literal does not match string`);
+    return assertions;
 }
 function comment_element() {
-	let expected = "<!-- Hello! -->";
-	let assertions = [];
-	let steps = compose(
-		new HtmlRules(),
-		`
+    let expected = "<!-- Hello! -->";
+    let assertions = [];
+    let steps = compose(new HtmlRules(), `
 		<!-- Hello! -->
-		`,
-	);
-	for (let step of steps.steps) {
-		console.log("step:", step);
-	}
-	let results = coyote.render(acs.comment_element());
-	let templateAssertion = assert(expected, results);
-	if (templateAssertion) assertions.push(templateAssertion);
-	let literal_results = coyote.render(acsl.comment_element());
-	let literalAssertion = assert(expected, literal_results);
-	if (literalAssertion) assertions.push(literalAssertion);
-	if (results[0] !== literal_results[0])
-		assertions.push(`literal does not match string`);
-	return assertions;
+		`);
+    for (let step of steps.steps) {
+        console.log("step:", step);
+    }
+    let results = coyote.render(acs.comment_element());
+    let templateAssertion = assert(expected, results);
+    if (templateAssertion)
+        assertions.push(templateAssertion);
+    let literal_results = coyote.render(acsl.comment_element());
+    let literalAssertion = assert(expected, literal_results);
+    if (literalAssertion)
+        assertions.push(literalAssertion);
+    if (results[0] !== literal_results[0])
+        assertions.push(`literal does not match string`);
+    return assertions;
 }
 export const tests = [
-	text_element,
-	empty_element,
-	fragment,
-	block_element_with_text,
-	inline_element_with_text,
-	void_element,
-	void_element_with_self_closing,
-	non_void_element,
-	comment_element,
+    text_element,
+    empty_element,
+    fragment,
+    block_element_with_text,
+    inline_element_with_text,
+    void_element,
+    void_element_with_self_closing,
+    non_void_element,
+    comment_element,
 ];
 export const options = {
-	title: import.meta.url,
+    title: import.meta.url,
 };
--- tests/atomics_component_set.js
+++ tests/atomics_component_set.js
@@ -1,147 +1,105 @@
 import { tmplStr } from "../dist/mod.js";
 export function text_element() {
-	return tmplStr(
-		`
+    return tmplStr(`
 
 			Beasts tread
 				softly   underfoot.
 
-		`,
-		[],
-	);
+		`, []);
 }
 export function empty_element() {
-	return tmplStr(
-		` 
+    return tmplStr(` 
 		<p>
 		</p>
-		`,
-		[],
-	);
+		`, []);
 }
 export function fragment() {
-	return tmplStr(
-		`
+    return tmplStr(`
 		<>
 		</>
-		`,
-		[],
-	);
+		`, []);
 }
 export function block_element_with_text() {
-	return tmplStr(
-		`
+    return tmplStr(`
 		<p>
 			hello!
 		</p>
-		`,
-		[],
-	);
+		`, []);
 }
 export function block_element_with_text_for_string() {
-	return tmplStr(
-		`
+    return tmplStr(`
 		<p>
 			hello!
 		</p>
-		`,
-		[],
-	);
+		`, []);
 }
 export function inline_element_with_text() {
-	return tmplStr(
-		`
+    return tmplStr(`
 		<b> hello! </b>
-		`,
-		[],
-	);
+		`, []);
 }
 export function void_element() {
-	return tmplStr(
-		`
+    return tmplStr(`
 		<input>
-		`,
-		[],
-	);
+		`, []);
 }
 export function void_element_with_self_closing() {
-	return tmplStr(
-		`
+    return tmplStr(`
 		<input />
-		`,
-		[],
-	);
+		`, []);
 }
 export function non_void_element() {
-	return tmplStr(
-		`
+    return tmplStr(`
 		<p />
-		`,
-		[],
-	);
+		`, []);
 }
 // needs updating
 export function comment_element() {
-	return tmplStr(
-		`
+    return tmplStr(`
 		<!-- Hello! -->
-		`,
-		[],
-	);
+		`, []);
 }
 export function alt_text_element() {
-	return tmplStr(
-		`<style>#woof .bark {
+    return tmplStr(`<style>#woof .bark {
 			color: doggo;
-		}</style>`,
-		[],
-	);
+		}</style>`, []);
 }
 export function alt_element_has_no_descendants() {
-	return tmplStr(
-		`
+    return tmplStr(`
 		<script>
 			{}
 		</script>
-		`,
-		[],
-	);
+		`, []);
 }
 export function preserved_text_element_retains_spacing() {
-	return tmplStr(
-		`
+    return tmplStr(`
 <pre>
 	U w U
 	  woof woof!
 </pre>
-		`,
-		[],
-	);
+		`, []);
 }
 export function attribute() {
-	return tmplStr("<span hai>UwU</span>", []);
+    return tmplStr("<span hai>UwU</span>", []);
 }
 export function attribute_with_single_quote() {
-	return tmplStr("<span hai=''>UwU</span>", []);
+    return tmplStr("<span hai=''>UwU</span>", []);
 }
 export function attribute_with_double_quote() {
-	return tmplStr('<span hai="">UwU</span>', []);
+    return tmplStr('<span hai="">UwU</span>', []);
 }
 export function attribute_with_single_quote_value() {
-	return tmplStr("<span hai='hewoo'>UwU</span>", []);
+    return tmplStr("<span hai='hewoo'>UwU</span>", []);
 }
 export function attribute_with_double_quote_value() {
-	return tmplStr('<span hai="hewoo">UwU</span>', []);
+    return tmplStr('<span hai="hewoo">UwU</span>', []);
 }
 export function banned_attribute() {
-	return tmplStr("<span onkeypress>UwU</span>", []);
+    return tmplStr("<span onkeypress>UwU</span>", []);
 }
 export function banned_attribute_quoted() {
-	return tmplStr("<span onclick=\"console.log('danger!')\">UwU</span>", []);
+    return tmplStr("<span onclick=\"console.log('danger!')\">UwU</span>", []);
 }
 export function banned_attribute_single_quoted() {
-	return tmplStr(
-		`<span onbegonia='\nconsole.log(\"BEGONIA!\")\n'>UwU</span>`,
-		[],
-	);
+    return tmplStr(`<span onbegonia='\nconsole.log(\"BEGONIA!\")\n'>UwU</span>`, []);
 }
